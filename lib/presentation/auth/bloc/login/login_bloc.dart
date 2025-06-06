3bd7c186bb731da260e1280ed7fb49a47931f233
WisnuDwiSeisno <wseisno37@gmail.com>
2025-06-06 22:49:19 +0700

    membuat login bloc

---
+++ lib/presentation/auth/bloc/login/login_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:canary_template/data/repository/auth_repository.dart';
+import 'package:canary_template/presentation/auth/bloc/login/login_event.dart';
+import 'package:canary_template/presentation/auth/bloc/login/login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final AuthRepository authRepository;
+
+  LoginBloc({required this.authRepository}) : super(LoginInitial()) {
+    on<LoginRequested>(_onLoginRequested);
+  }
+
+  Future<void> _onLoginRequested(
+    LoginRequested event,
+    Emitter<LoginState> emit,
+  ) async {
+    emit(LoginLoading());
+
+    final result = await authRepository.login(event.requestModel);
+
+    result.fold(
+      (l) => emit(LoginFailure(error: l)),
+      (r) => emit(LoginSuccess(responseModel: r)),
+    );
+  }
+}
