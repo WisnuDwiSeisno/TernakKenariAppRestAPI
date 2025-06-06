096788b58c807ee3c41ae01b435dbffc3fa77341
WisnuDwiSeisno <wseisno37@gmail.com>
2025-06-06 22:54:58 +0700

    membuat register bloc

---
+++ lib/presentation/auth/bloc/register/register_bloc.dart
@@ -0,0 +1,24 @@
+import 'package:canary_template/data/repository/auth_repository.dart';
+import 'package:canary_template/presentation/auth/bloc/register/register_state.dart';
+
+class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
+  final AuthRepository authRepository;
+
+  RegisterBloc({required this.authRepository}) : super(RegisterInitial()) {
+    on<RegisterRequested>(_onRegisterRequested);
+  }
+
+  Future<void> _onRegisterRequested(
+    RegisterRequested event,
+    Emitter<RegisterState> emit,
+  ) async {
+    emit(RegisterLoading());
+
+    final result = await authRepository.register(event.requestModel);
+
+    result.fold(
+      (l) => emit(RegisterFailure(error: l)),
+      (r) => emit(RegisterSuccess(message: r)),
+    );
+  }
+}
